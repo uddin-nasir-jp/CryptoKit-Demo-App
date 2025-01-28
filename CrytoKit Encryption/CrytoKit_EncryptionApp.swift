b6acf283120b30d5509b8c4ae5d39b0f0a345442
Nasir Uddin <uddin.nasir.jp@gmail.com>
2025-01-28 20:32:33 +0900

    Initial Commit

---
+++ CrytoKit Encryption/CrytoKit_EncryptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrytoKit_EncryptionApp.swift
+//  CrytoKit Encryption
+//
+//  Created by Nasir Uddin on 28/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CrytoKit_EncryptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
